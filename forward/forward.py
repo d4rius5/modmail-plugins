7752400fdc045b61a87c3946dec2ed10468af0a9
d4rius5 <dir3us@gmail.com>
2026-05-02 02:45:26 +0300

    Update forward.py

--- forward/forward.py
+++ forward/forward.py
@@ -12,25 +12,26 @@ class Forward(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
     @checks.thread_only()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
-
-    @commands.command()
     async def rename(self, ctx, *, msg):
-      try:
-          channel = await bot.fetch_channel(1277792057632493591)
+        try:
+            channel = await self.bot.fetch_channel(1277792057632493591)
 
-          files = []
-          for attachment in ctx.message.attachments:
-              file = await attachment.to_file()
-              files.append(file)
+            files = []
+            for attachment in ctx.message.attachments:
+                file = await attachment.to_file()
+                files.append(file)
 
-          await channel.send(msg, files=files)
+            await channel.send(msg, files=files)
 
-          await ctx.message.add_reaction("✅")
-      except Exception as e:
-          await ctx.message.add_reaction("❌")
-          await ctx.reply(e)
+            await ctx.message.add_reaction("✅")
+
+        except Exception as e:
+            await ctx.message.add_reaction("❌")
+            await ctx.reply(str(e))
+
 
 async def setup(bot):
     await bot.add_cog(Forward(bot))
