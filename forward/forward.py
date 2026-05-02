da1cefb8b4ffb2edff0a55859ba452e2151db9dd
d4rius5 <dir3us@gmail.com>
2026-05-02 03:04:19 +0300

    Update and rename forward.py to forward/forward.py

--- forward/forward.py
+++ forward/forward.py
@@ -1,23 +1,13 @@
-import discord
 from discord.ext import commands
 
-from core import checks
-from core.models import PermissionLevel
-
-import datetime
-
 class Forward(commands.Cog):
-    """Forward a message!"""
-
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    @checks.thread_only()
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    async def rename(self, ctx, *, msg):
+    async def f(self, ctx, *, msg):
         try:
-            channel = await self.bot.fetch_channel(1277792057632493591)
+            channel = await bot.fetch_channel(1277792057632493591)
 
             files = []
             for attachment in ctx.message.attachments:
@@ -27,11 +17,9 @@ class Forward(commands.Cog):
             await channel.send(msg, files=files)
 
             await ctx.message.add_reaction("✅")
-
         except Exception as e:
             await ctx.message.add_reaction("❌")
-            await ctx.reply(str(e))
-
+            await ctx.reply(e)
 
 async def setup(bot):
-    await bot.add_cog(Forward(bot))
+    await bot.add_cog(Hello(bot))
