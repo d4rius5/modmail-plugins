186cfba4417530f42664de6e582ec71c9e8478b9
d4rius5 <dir3us@gmail.com>
2026-05-02 03:18:24 +0300

    Update forward.py

--- forward/forward.py
+++ forward/forward.py
@@ -6,10 +6,11 @@ class Forward(commands.Cog):
 
     @commands.command()
     async def f(self, ctx, *, msg):
-        if ctx.channel != 1437884513702248519:
+        if ctx.channel.id != 1437884513702248519:
             return
+
         try:
-            channel = await bot.fetch_channel(1277792057632493591)
+            channel = await self.bot.fetch_channel(1277792057632493591)
 
             files = []
             for attachment in ctx.message.attachments:
@@ -19,9 +20,11 @@ class Forward(commands.Cog):
             await channel.send(msg, files=files)
 
             await ctx.message.add_reaction("✅")
+
         except Exception as e:
             await ctx.message.add_reaction("❌")
-            await ctx.reply(e)
+            await ctx.reply(str(e))
+
 
 async def setup(bot):
     await bot.add_cog(Forward(bot))
